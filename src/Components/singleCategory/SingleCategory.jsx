092d2805d0d553f54229a0586ae608692f9168ea
justina177 <justinaenenche@yahoo.com>
2023-08-25 19:00:36 +0100

    added folder structure

---
+++ src/Components/singleCategory/SingleCategory.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SingleCategory.scss';
+
+const SingleCategory = () => {
+  return (
+    <div>SingleCategory</div>
+  )
+}
+
+export default SingleCategory
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./homePage/HomePage";
+import Category from "./categoryPage/CategoryPage";
+import Cart from "./cartPage/CartPage";
+
+export {Home, Category, Cart};
